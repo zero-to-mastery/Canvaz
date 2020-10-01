f830aa2bd5613a66de26b168903526c8ae9855d4
christine-aqui <aqui.christine@gmail.com>
2020-09-30 21:32:11 -0400

    my art submission

--- include.js
+++ include.js
@@ -1,12 +1,19 @@
 let cards = [
-  //  Add your card in this section
-  {
-    artName: 'welcome robot',
-    pageLink: './Art/joy/robot.html',
-    imageLink: './Art/joy/robot.png',
-    author: 'joy',
-    githubLink: 'https://github.com/royranger',
-  },
+	//  Add your card in this section
+	{
+		artName: 'welcome robot',
+		pageLink: './Art/joy/robot.html',
+		imageLink: './Art/joy/robot.png',
+		author: 'joy',
+		githubLink: 'https://github.com/royranger',
+	},
+	{
+		artName: 'This is Arttttttt',
+		pageLink: './Art/christine-aqui/caqui.html',
+		imageLink: '/Art/christine-aqui/caqui.png',
+		author: 'christine-aqui',
+		githubLink: 'https://github.com/christine-aqui',
+	},
 ];
 
 // +--------------------------------------------------------------------------------+
@@ -19,26 +26,26 @@ let cards = [
 // You don't need to modify this
 let contents = [];
 Shuffle(cards).forEach((c) => {
-  contents.push([
-    `<li class="card">` +
-      `<a href='${c.pageLink}'>` +
-      `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
-      `</a>` +
-      `<div class="flex-content">` +
-      `<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
-      `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
-      `</div>` +
-      `</li>`,
-  ]);
+	contents.push([
+		`<li class="card">` +
+		`<a href='${c.pageLink}'>` +
+		`<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
+		`</a>` +
+		`<div class="flex-content">` +
+		`<a href='${c.pageLink}'><h3 class="art-title">${c.artName}</h3></a>` +
+		`<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
+		`</div>` +
+		`</li>`,
+	]);
 });
 
 document.getElementById('cards').innerHTML = contents.join('');
 
 function Shuffle(o) {
-  for (
-    var j, x, i = o.length;
-    i;
-    j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
-  );
-  return o;
+	for (
+		var j, x, i = o.length;
+		i;
+		j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+	);
+	return o;
 }
