dd130d554ca23719c76f6d28bc9b14910bf3a4b2
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2020-10-01 16:14:17 +0000

    (CI) format code

--- Art/ravi/script.js
+++ Art/ravi/script.js
@@ -2,13 +2,13 @@ const canvas = document.getElementById('ravi');
 
 const jw = canvas.getContext('2d');
 
-jw.fillStyle = 'black'
+jw.fillStyle = 'black';
 jw.fillRect(0, 0, 400, 100);
 
-jw.fillStyle = 'red'
+jw.fillStyle = 'red';
 jw.fillRect(0, 100, 400, 100);
 
-jw.fillStyle = '#ffcc00'
+jw.fillStyle = '#ffcc00';
 jw.fillRect(0, 200, 400, 100);
 
 jw.stroke();
