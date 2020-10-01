69e33225f4a7b1dee4487907dfa58a9e6588c20a
Jappanjeet Singh <42716542+Jappan07@users.noreply.github.com>
2020-10-01 21:59:39 +0530

    adding Snake Game made by Jappan (#16)

---
+++ Art/jappan/snake.js
@@ -0,0 +1,164 @@
+// snakes game
+
+// Game loop - Init, Draw, Update
+
+function init() {
+  // console.log("Init")
+  canvas = document.getElementById('mycanvas');
+  pen = canvas.getContext('2d'); //pen used for drawing on board
+  W = canvas.width;
+  H = canvas.height;
+  score = 1;
+  game_over = false;
+
+  food_img = new Image();
+  food_img.src = 'assets/apple.png';
+
+  trophy = new Image();
+  trophy.src = 'assets/trophy.png';
+
+  food = getRandomFood();
+
+  snake = {
+    init_length: 1,
+    color: 'blue',
+    cells: [],
+    direction: 'right',
+
+    createSnake: function () {
+      for (var i = this.init_length - 1; i >= 0; i--) {
+        this.cells.push({ x: i, y: 0 });
+      }
+    },
+    drawSnake: function () {
+      for (var i = 0; i < this.cells.length; i++) {
+        pen.fillStyle = this.color;
+        pen.strokeStyle = 'black';
+        pen.lineWidth = 2;
+        pen.strokeRect(this.cells[i].x * 10, this.cells[i].y * 10, 10, 10);
+        pen.fillRect(this.cells[i].x * 10, this.cells[i].y * 10, 10, 10);
+      }
+    },
+    updateSnake: function () {
+      var headX = this.cells[0].x;
+      var headY = this.cells[0].y;
+
+      if (headX == food.x && headY == food.y) {
+        food = getRandomFood();
+        score++;
+      } else {
+        // Pop last cell if food is not eaten
+        this.cells.pop();
+      }
+
+      if (this.direction == 'right') {
+        nextX = headX + 1;
+        nextY = headY;
+      } else if (this.direction == 'left') {
+        nextX = headX - 1;
+        nextY = headY;
+      } else if (this.direction == 'down') {
+        nextX = headX;
+        nextY = headY + 1;
+      } else {
+        nextX = headX;
+        nextY = headY - 1;
+      }
+      // Insert the new cell at head/front
+      this.cells.unshift({ x: nextX, y: nextY });
+
+      var last_x = Math.round(W / 10);
+      var last_y = Math.round(H / 10);
+
+      for (var i = 1; i < snake.cells.length; i++) {
+        if (
+          this.cells[0].x == this.cells[i].x &&
+          this.cells[0].y == this.cells[i].y
+        ) {
+          alert('GameOver!');
+          game_over = true;
+        }
+      }
+
+      if (
+        this.cells[0].y < 0 ||
+        this.cells[0].x < 0 ||
+        this.cells[0].x > last_x ||
+        this.cells[0].y > last_y
+      ) {
+        alert('GameOver!');
+        game_over = true;
+      }
+    },
+  };
+  snake.createSnake();
+
+  // Add Event listeners to our game
+  // listen for keyboard events.
+  function keyPressed(e) {
+    console.log('Key pressed');
+    console.log(e);
+
+    if (e.key == 'ArrowRight') {
+      snake.direction = 'right';
+    } else if (e.key == 'ArrowLeft') {
+      snake.direction = 'left';
+    } else if (e.key == 'ArrowDown') {
+      snake.direction = 'down';
+    } else if (e.key == 'ArrowUp') {
+      snake.direction = 'up';
+    }
+  }
+
+  document.addEventListener('keydown', keyPressed);
+}
+
+function draw() {
+  pen.clearRect(0, 0, W, H);
+  // draw the snake
+  snake.drawSnake();
+
+  // draw the food
+  pen.fillStyle = food.color;
+  pen.drawImage(food_img, food.x * 10, food.y * 10, 10, 10);
+
+  // draw score on screen
+  pen.drawImage(trophy, 60, 2, 12, 12);
+  pen.fillStyle = 'white';
+  pen.font = '12px Roboto';
+  pen.fillText('Score : ' + score, 10, 10);
+}
+
+function update() {
+  snake.updateSnake();
+}
+
+function getRandomFood() {
+  var foodX = Math.round((Math.random() * (W - 10)) / 10);
+  var foodY = Math.round((Math.random() * (H - 10)) / 10);
+
+  foodColors = ['red', 'green', 'aqua', 'coral', 'orchid'];
+  var i = Math.round(Math.random() * foodColors.length);
+
+  var food = {
+    x: foodX,
+    y: foodY,
+    color: foodColors[i],
+  };
+  return food;
+}
+
+function gameloop() {
+  draw();
+  update();
+
+  if (game_over == true) {
+    clearInterval(f);
+    return;
+  }
+}
+
+const mybut = document.querySelector('.mybutton-btn');
+mybut.onclick = init;
+// Call Game Loop after t time
+var f = setInterval(gameloop, 100);
--- include.js
+++ include.js
@@ -5,63 +5,70 @@ let cards = [
     pageLink: './Art/joy/robot.html',
     imageLink: './Art/joy/robot.png',
     author: 'joy',
-    githubLink: 'https://github.com/royranger',
+    githubLink: 'https://github.com/royranger'
   },
   {
     artName: 'lollipop',
     pageLink: './Art/ctq3103/lollipop.html',
     imageLink: './Art/ctq3103/lollipop.png',
     author: 'ctq3103',
-    githubLink: 'https://github.com/ctq3103',
+    githubLink: 'https://github.com/ctq3103'
   },
   {
     artName: 'This is Arttttttt',
     pageLink: './Art/christine-aqui/caqui.html',
     imageLink: './Art/christine-aqui/caqui.png',
     author: 'christine-aqui',
-    githubLink: 'https://github.com/christine-aqui',
+    githubLink: 'https://github.com/christine-aqui'
   },
   {
     artName: 'World of machine learning',
     pageLink: './Art/vivek2509/machine_learning.html',
     imageLink: './Art/vivek2509/machine_learning.png',
     author: 'Vivek2509',
-    githubLink: 'https://github.com/Vivek2509',
+    githubLink: 'https://github.com/Vivek2509'
   },
   {
     artName: 'Hacktober Bannner',
     pageLink: './Art/antonio-trajanovski/index.html',
     imageLink: './Art/antonio-trajanovski/banner.png',
     author: 'Antonio Trajanovski',
-    githubLink: 'https://github.com/antoniotrajanovski',
+    githubLink: 'https://github.com/antoniotrajanovski'
   },
   {
     artName: '3 bouncing balls',
     pageLink: './Art/shamika-kumar/index.html',
     imageLink: './Art/shamika-kumar/shamika-art.gif',
     author: 'shamika-kumar',
-    githubLink: 'https://github.com/shamikakumar',
+    githubLink: 'https://github.com/shamikakumar'
   },
   {
     artName: 'Flower',
     pageLink: './Art/Neha/index.html',
     imageLink: './Art/Neha/flower.png',
     author: 'Neha',
-    githubLink: 'https://github.com/Neha045',
+    githubLink: 'https://github.com/Neha045'
   },
   {
     artName: 'Diamond',
     pageLink: './Art/ab510/diamond.html',
     imageLink: './Art/ab510/diamond.png',
     author: 'ab510',
-    githubLink: 'https://github.com/ab510',
+    githubLink: 'https://github.com/ab510'
+  },
+  {
+    artName: 'Snake Game',
+    pageLink: './Art/jappan/index.html',
+    imageLink: './Art/jappan/snake-game.gif',
+    author: 'Jappan',
+    githubLink: 'https://github.com/Jappan07'
   },
   {
     artName: 'The German Flag',
     pageLink: './Art/ravi/index.html',
     imageLink: './Art/ravi/German_Flag.jpg',
     author: 'Ravi',
-    githubLink: 'https://github.com/rav291',
+    githubLink: 'https://github.com/rav291'
   },
 ];
 
