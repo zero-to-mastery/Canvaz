d6134b6bdf3a092191ffb058a48cd5abdcd65727
ctq3103 <56945989+ctq3103@users.noreply.github.com>
2020-10-02 12:14:23 +0700

    added my canvas (#36)

--- include.js
+++ include.js
@@ -141,12 +141,19 @@ let cards = [
     githubLink: 'https://github.com/gh0sttttt'
   },
   {
-		artName: 'Color Wall Builder',
-		pageLink: './Art/Jamiu/index.html',
-		imageLink: './Art/Jamiu/color.PNG',
-		author: 'Jamiu',
-		githubLink: 'https://github.com/JamiuJimoh'
-	}
+    artName: 'Color Wall Builder',
+    pageLink: './Art/Jamiu/index.html',
+    imageLink: './Art/Jamiu/color.PNG',
+    author: 'Jamiu',
+    githubLink: 'https://github.com/JamiuJimoh'
+  },
+  {
+    artName: 'Particles ZTM',
+    pageLink: './Art/ctq3103/particles-ztm.html',
+    imageLink: './Art/ctq3103/particles-ztm.gif',
+    author: 'ctq3103',
+    githubLink: 'https://github.com/ctq3103'
+  }
 ];
 
 // +--------------------------------------------------------------------------------+
@@ -159,22 +166,26 @@ let cards = [
 // You don't need to modify this
 let contents = [];
 Shuffle(cards).forEach((c) => {
-	contents.push([
-		`<li class="card">` +
-			`<a href='${c.pageLink}' target="_blank">` +
-			`<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
-			`</a>` +
-			`<div class="flex-content">` +
-			`<a href='${c.pageLink}' target="_blank"><h3 class="art-title">${c.artName}</h3></a>` +
-			`<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
-			`</div>` +
-			`</li>`
-	]);
+  contents.push([
+    `<li class="card">` +
+      `<a href='${c.pageLink}' target="_blank">` +
+      `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
+      `</a>` +
+      `<div class="flex-content">` +
+      `<a href='${c.pageLink}' target="_blank"><h3 class="art-title">${c.artName}</h3></a>` +
+      `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
+      `</div>` +
+      `</li>`
+  ]);
 });
 
 document.getElementById('cards').innerHTML = contents.join('');
 
 function Shuffle(o) {
-	for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-	return o;
+  for (
+    var j, x, i = o.length;
+    i;
+    j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+  );
+  return o;
 }
