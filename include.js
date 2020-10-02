eec7e0ebaef6a81f4ac8d65d50c6909f6647c22d
Anette <anet1@rocketmail.com>
2020-10-02 22:27:25 +0100

    added my art (#68)

--- include.js
+++ include.js
@@ -1,286 +1,286 @@
 let cards = [
-	//  Add your card in this section
-	{
-		artName: 'Monochromat your Photo',
-		pageLink: './Art/Shashvat/index.html',
-		imageLink: './Art/Shashvat/shashvat-sc.png',
-		author: 'Iamshashvat',
-		githubLink: 'https://github.com/Iamshashvat'
-	},
-	{
-		artName: 'Welocme To Hacktoberfest',
-		pageLink: './Art/Utkarsh2604/index.html',
-		imageLink: './Art/Utkarsh2604/Screenshot.png',
-		author: 'Utkarsh',
-		githubLink: 'https://github.com/Utkarsh2604'
-	},
-	{
-		artName: 'Welocme To Hacktoberfest',
-		pageLink: './Art/shivam/index.html',
-		imageLink: './Art/shivem/loading.PNG',
-		author: 'shivam',
-		githubLink: 'https://github.com/shivam12k'
-	},
-	{
-		artName: 'Japan',
-		pageLink: './Art/kelvinchan2406/index.html',
-		imageLink: './Art/kelvinchan2406/japan.png',
-		author: 'kelvinchan2406',
-		githubLink: 'https://github.com/kelvinchan2406'
-	},
-	{
-		artName: 'welcome robot',
-		pageLink: './Art/joy/robot.html',
-		imageLink: './Art/joy/robot.png',
-		author: 'joy',
-		githubLink: 'https://github.com/royranger'
-	},
-	{
-		artName: 'ZTM Number One',
-		pageLink: './Art/hieutrinh/index.html',
-		imageLink: './Art/hieutrinh/zerotomasteryone.png',
-		author: 'hieutrinh',
-		githubLink: 'https://github.com/trinhminhieu'
-	},
-	{
-		artName: 'lollipop',
-		pageLink: './Art/ctq3103/lollipop.html',
-		imageLink: './Art/ctq3103/lollipop.png',
-		author: 'ctq3103',
-		githubLink: 'https://github.com/ctq3103'
-	},
-	{
-		artName: 'This is Arttttttt',
-		pageLink: './Art/christine-aqui/caqui.html',
-		imageLink: './Art/christine-aqui/caqui.png',
-		author: 'christine-aqui',
-		githubLink: 'https://github.com/christine-aqui'
-	},
-	{
-		artName: 'World of machine learning',
-		pageLink: './Art/vivek2509/machine_learning.html',
-		imageLink: './Art/vivek2509/machine_learning.png',
-		author: 'Vivek2509',
-		githubLink: 'https://github.com/Vivek2509'
-	},
-	{
-		artName: 'Hacktober Bannner',
-		pageLink: './Art/antonio-trajanovski/index.html',
-		imageLink: './Art/antonio-trajanovski/banner.png',
-		author: 'Antonio Trajanovski',
-		githubLink: 'https://github.com/antoniotrajanovski'
-	},
-	{
-		artName: '3 bouncing balls',
-		pageLink: './Art/shamika-kumar/index.html',
-		imageLink: './Art/shamika-kumar/shamika-art.gif',
-		author: 'shamika-kumar',
-		githubLink: 'https://github.com/shamikakumar'
-	},
-	{
-		artName: 'Flower',
-		pageLink: './Art/Neha/index.html',
-		imageLink: './Art/Neha/flower.png',
-		author: 'Neha',
-		githubLink: 'https://github.com/Neha045'
-	},
-	{
-		artName: 'Brazilian Flag',
-		pageLink: './Art/cassio/index.html',
-		imageLink: './Art/cassio/brazil.png',
-		author: 'Cassio Groh',
-		githubLink: 'https://github.com/cassiogroh'
-	},
-	{
-		artName: 'Blackboard',
-		pageLink: './Art/preity-p/index.html',
-		imageLink: './Art/preity-p/in-motion.gif',
-		author: 'Preity Priya',
-		githubLink: 'https://github.com/preity-p'
-	},
-	{
-		artName: 'Diamond',
-		pageLink: './Art/ab510/diamond.html',
-		imageLink: './Art/ab510/diamond.png',
-		author: 'ab510',
-		githubLink: 'https://github.com/ab510'
-	},
-	{
-		artName: 'Snake Game',
-		pageLink: './Art/jappan/index.html',
-		imageLink: './Art/jappan/snake-game.gif',
-		author: 'Jappan',
-		githubLink: 'https://github.com/Jappan07'
-	},
-	{
-		artName: 'The German Flag',
-		pageLink: './Art/ravi/index.html',
-		imageLink: './Art/ravi/German_Flag.jpg',
-		author: 'Ravi',
-		githubLink: 'https://github.com/rav291'
-	},
-	{
-		artName: 'Hactoberfest 2020',
-		pageLink: './Art/velada/index.html',
-		imageLink: './Art/velada/canvas.png',
-		author: 'velada',
-		githubLink: 'https://github.com/annima24'
-	},
-	{
-		artName: 'Our Indian Flag',
-		pageLink: './Art/aman/index.html',
-		imageLink: './Art/aman/screenshot.jpg',
-		author: 'aman',
-		githubLink: 'https://github.com/aman339'
-	},
-	{
-		artName: 'Bubbles',
-		pageLink: './Art/michaeljoy255/bubbles.html',
-		imageLink: './Art/michaeljoy255/bubbles.png',
-		author: 'Michael J',
-		githubLink: 'https://github.com/michaeljoy255'
-	},
-	{
-		artName: 'My Little Piece of Art',
-		pageLink: './Art/gincos/index.html',
-		imageLink: './Art/gincos/finished.png',
-		author: 'Gincos',
-		githubLink: 'https://github.com/gincos'
-	},
-	{
-		artName: 'ZTM Hacktoberfest',
-		pageLink: './Art/sherine/index.html',
-		imageLink: './Art/sherine/ztm.png',
-		author: 'Sherine',
-		githubLink: 'https://github.com/sherinechally'
-	},
-	{
-		artName: 'Hactoberfest2020',
-		pageLink: './Art/krittiya-clark/index.html',
-		imageLink: './Art/krittiya-clark/Hactoberfest2020.png',
-		author: 'Krittiya Clark',
-		githubLink: 'https://github.com/krittiyaclark'
-	},
-	{
-		artName: 'Random monochromatic palette',
-		pageLink: './Art/carls13/index.html',
-		imageLink: './Art/carls13/mono.png',
-		author: 'Carls13',
-		githubLink: 'https://github.com/carls13'
-	},
-	{
-		artName: 'Cool cursor',
-		pageLink: './Art/ashwin/Cool-cursor.html',
-		imageLink: './Art/ashwin/Cool-cursor.png',
-		author: 'ashwin',
-		githubLink: 'https://github.com/AswinBarath'
-	},
-	{
-		artName: 'smile',
-		pageLink: './Art/gh0sttttt/gh0sttttt.html',
-		imageLink: './Art/gh0sttttt/smile-hacktoberfest.gif',
-		author: 'Aaron',
-		githubLink: 'https://github.com/gh0sttttt'
-	},
-	{
-		artName: 'Color Wall Builder',
-		pageLink: './Art/Jamiu/index.html',
-		imageLink: './Art/Jamiu/color.PNG',
-		author: 'Jamiu',
-		githubLink: 'https://github.com/JamiuJimoh'
-	},
-	{
-		artName: 'Russia Flag',
-		pageLink: './Art/Rohit/index.html',
-		imageLink: './Art/Rohit/russia_flag.jpg',
-		author: 'Rohit Ahuja',
-		githubLink: 'https://github.com/rohit1204'
-	},
-	{
-		artName: 'Particles ZTM',
-		pageLink: './Art/ctq3103/particles-ztm.html',
-		imageLink: './Art/ctq3103/particles-ztm.gif',
-		author: 'ctq3103',
-		githubLink: 'https://github.com/ctq3103'
-	},
-	{
-		artName: 'Hacktoberfest>Olympics',
-		pageLink: './Art/nnhhiilliisstt/index.html',
-		imageLink: './Art/nnhhiilliisstt/logo.png',
-		author: 'Kumar Aditya',
-		githubLink: 'https://github.com/nnhhiilliisstt'
-	},
-	{
-		artName: 'Indian Flag',
-		pageLink: './Art/Bmg/index.html',
-		imageLink: './Art/Bmg/India.png',
-		author: 'Bhavansh',
-		githubLink: 'https://github.com/bhavansh'
-	},
+  //  Add your card in this section
+  {
+    artName: 'Monochromat your Photo',
+    pageLink: './Art/Shashvat/index.html',
+    imageLink: './Art/Shashvat/shashvat-sc.png',
+    author: 'Iamshashvat',
+    githubLink: 'https://github.com/Iamshashvat'
+  },
+  {
+    artName: 'Welocme To Hacktoberfest',
+    pageLink: './Art/Utkarsh2604/index.html',
+    imageLink: './Art/Utkarsh2604/Screenshot.png',
+    author: 'Utkarsh',
+    githubLink: 'https://github.com/Utkarsh2604'
+  },
+  {
+    artName: 'Welocme To Hacktoberfest',
+    pageLink: './Art/shivam/index.html',
+    imageLink: './Art/shivem/loading.PNG',
+    author: 'shivam',
+    githubLink: 'https://github.com/shivam12k'
+  },
+  {
+    artName: 'Japan',
+    pageLink: './Art/kelvinchan2406/index.html',
+    imageLink: './Art/kelvinchan2406/japan.png',
+    author: 'kelvinchan2406',
+    githubLink: 'https://github.com/kelvinchan2406'
+  },
+  {
+    artName: 'welcome robot',
+    pageLink: './Art/joy/robot.html',
+    imageLink: './Art/joy/robot.png',
+    author: 'joy',
+    githubLink: 'https://github.com/royranger'
+  },
+  {
+    artName: 'ZTM Number One',
+    pageLink: './Art/hieutrinh/index.html',
+    imageLink: './Art/hieutrinh/zerotomasteryone.png',
+    author: 'hieutrinh',
+    githubLink: 'https://github.com/trinhminhieu'
+  },
+  {
+    artName: 'lollipop',
+    pageLink: './Art/ctq3103/lollipop.html',
+    imageLink: './Art/ctq3103/lollipop.png',
+    author: 'ctq3103',
+    githubLink: 'https://github.com/ctq3103'
+  },
+  {
+    artName: 'This is Arttttttt',
+    pageLink: './Art/christine-aqui/caqui.html',
+    imageLink: './Art/christine-aqui/caqui.png',
+    author: 'christine-aqui',
+    githubLink: 'https://github.com/christine-aqui'
+  },
+  {
+    artName: 'World of machine learning',
+    pageLink: './Art/vivek2509/machine_learning.html',
+    imageLink: './Art/vivek2509/machine_learning.png',
+    author: 'Vivek2509',
+    githubLink: 'https://github.com/Vivek2509'
+  },
+  {
+    artName: 'Hacktober Bannner',
+    pageLink: './Art/antonio-trajanovski/index.html',
+    imageLink: './Art/antonio-trajanovski/banner.png',
+    author: 'Antonio Trajanovski',
+    githubLink: 'https://github.com/antoniotrajanovski'
+  },
+  {
+    artName: '3 bouncing balls',
+    pageLink: './Art/shamika-kumar/index.html',
+    imageLink: './Art/shamika-kumar/shamika-art.gif',
+    author: 'shamika-kumar',
+    githubLink: 'https://github.com/shamikakumar'
+  },
+  {
+    artName: 'Flower',
+    pageLink: './Art/Neha/index.html',
+    imageLink: './Art/Neha/flower.png',
+    author: 'Neha',
+    githubLink: 'https://github.com/Neha045'
+  },
+  {
+    artName: 'Brazilian Flag',
+    pageLink: './Art/cassio/index.html',
+    imageLink: './Art/cassio/brazil.png',
+    author: 'Cassio Groh',
+    githubLink: 'https://github.com/cassiogroh'
+  },
+  {
+    artName: 'Blackboard',
+    pageLink: './Art/preity-p/index.html',
+    imageLink: './Art/preity-p/in-motion.gif',
+    author: 'Preity Priya',
+    githubLink: 'https://github.com/preity-p'
+  },
+  {
+    artName: 'Diamond',
+    pageLink: './Art/ab510/diamond.html',
+    imageLink: './Art/ab510/diamond.png',
+    author: 'ab510',
+    githubLink: 'https://github.com/ab510'
+  },
+  {
+    artName: 'Snake Game',
+    pageLink: './Art/jappan/index.html',
+    imageLink: './Art/jappan/snake-game.gif',
+    author: 'Jappan',
+    githubLink: 'https://github.com/Jappan07'
+  },
+  {
+    artName: 'The German Flag',
+    pageLink: './Art/ravi/index.html',
+    imageLink: './Art/ravi/German_Flag.jpg',
+    author: 'Ravi',
+    githubLink: 'https://github.com/rav291'
+  },
+  {
+    artName: 'Hactoberfest 2020',
+    pageLink: './Art/velada/index.html',
+    imageLink: './Art/velada/canvas.png',
+    author: 'velada',
+    githubLink: 'https://github.com/annima24'
+  },
+  {
+    artName: 'Our Indian Flag',
+    pageLink: './Art/aman/index.html',
+    imageLink: './Art/aman/screenshot.jpg',
+    author: 'aman',
+    githubLink: 'https://github.com/aman339'
+  },
+  {
+    artName: 'Bubbles',
+    pageLink: './Art/michaeljoy255/bubbles.html',
+    imageLink: './Art/michaeljoy255/bubbles.png',
+    author: 'Michael J',
+    githubLink: 'https://github.com/michaeljoy255'
+  },
+  {
+    artName: 'My Little Piece of Art',
+    pageLink: './Art/gincos/index.html',
+    imageLink: './Art/gincos/finished.png',
+    author: 'Gincos',
+    githubLink: 'https://github.com/gincos'
+  },
+  {
+    artName: 'ZTM Hacktoberfest',
+    pageLink: './Art/sherine/index.html',
+    imageLink: './Art/sherine/ztm.png',
+    author: 'Sherine',
+    githubLink: 'https://github.com/sherinechally'
+  },
+  {
+    artName: 'Hactoberfest2020',
+    pageLink: './Art/krittiya-clark/index.html',
+    imageLink: './Art/krittiya-clark/Hactoberfest2020.png',
+    author: 'Krittiya Clark',
+    githubLink: 'https://github.com/krittiyaclark'
+  },
+  {
+    artName: 'Random monochromatic palette',
+    pageLink: './Art/carls13/index.html',
+    imageLink: './Art/carls13/mono.png',
+    author: 'Carls13',
+    githubLink: 'https://github.com/carls13'
+  },
+  {
+    artName: 'Cool cursor',
+    pageLink: './Art/ashwin/Cool-cursor.html',
+    imageLink: './Art/ashwin/Cool-cursor.png',
+    author: 'ashwin',
+    githubLink: 'https://github.com/AswinBarath'
+  },
+  {
+    artName: 'smile',
+    pageLink: './Art/gh0sttttt/gh0sttttt.html',
+    imageLink: './Art/gh0sttttt/smile-hacktoberfest.gif',
+    author: 'Aaron',
+    githubLink: 'https://github.com/gh0sttttt'
+  },
+  {
+    artName: 'Color Wall Builder',
+    pageLink: './Art/Jamiu/index.html',
+    imageLink: './Art/Jamiu/color.PNG',
+    author: 'Jamiu',
+    githubLink: 'https://github.com/JamiuJimoh'
+  },
+  {
+    artName: 'Russia Flag',
+    pageLink: './Art/Rohit/index.html',
+    imageLink: './Art/Rohit/russia_flag.jpg',
+    author: 'Rohit Ahuja',
+    githubLink: 'https://github.com/rohit1204'
+  },
+  {
+    artName: 'Particles ZTM',
+    pageLink: './Art/ctq3103/particles-ztm.html',
+    imageLink: './Art/ctq3103/particles-ztm.gif',
+    author: 'ctq3103',
+    githubLink: 'https://github.com/ctq3103'
+  },
+  {
+    artName: 'Hacktoberfest>Olympics',
+    pageLink: './Art/nnhhiilliisstt/index.html',
+    imageLink: './Art/nnhhiilliisstt/logo.png',
+    author: 'Kumar Aditya',
+    githubLink: 'https://github.com/nnhhiilliisstt'
+  },
+  {
+    artName: 'Indian Flag',
+    pageLink: './Art/Bmg/index.html',
+    imageLink: './Art/Bmg/India.png',
+    author: 'Bhavansh',
+    githubLink: 'https://github.com/bhavansh'
+  },
 
-	{
-		artName: 'Bouncing Balls',
-		pageLink: './Art/varunv/canva.html',
-		imageLink: './Art/varunv/canva.gif',
-		author: 'Varun V',
-		githubLink: 'https://github.com/VarunV991'
-	},
-	{
-		artName: 'Russian Flag',
-		pageLink: './Art/AdityaSaroj/index.html',
-		imageLink: './Art/AdityaSaroj/russia.png',
-		author: 'Aditya Kumar Saroj',
-		githubLink: 'https://github.com/AdityaSaroj'
-	},
-	{
-		artName: 'Bauhaus Geometric',
-		pageLink: './Art/handa/index.html',
-		imageLink: './Art/handa/geometric.png',
-		author: 'Handa',
-		githubLink: 'https://github.com/handa26'
-	},
-	{
-		artName: 'Hacktoberfest rocks',
-		pageLink: './Art/cb/index.html',
-		imageLink: './Art/cb/hackto.png',
-		author: 'Chirag',
-		githubLink: 'https://github.com/bagdechirag'
-	},
-	{
-		artName: 'Gradient Hello World',
-		pageLink: './Art/mond/index.html',
-		imageLink: './Art/mond/pic.png',
-		author: 'Mond',
-		githubLink: 'https://github.com/monddelmundo'
-	},
-	{
-		artName: 'Bubble ZTM',
-		pageLink: './Art/ctq3103/bubble-ztm.html',
-		imageLink: './Art/ctq3103/bubble-ztm.gif',
-		author: 'ctq3103',
-		githubLink: 'https://github.com/ctq3103'
-	},
-	{
-		artName: 'Color Circle Anmination',
-		pageLink: './Art/tranjenny/index.html',
-		imageLink: './Art/tranjenny/theta.gif',
-		author: 'tranjenny',
-		githubLink: 'https://github.com/tranjenny'
-	},
-	{
-		artName: 'Spiraly Helix',
-		pageLink: './Art/hmahajan99/index.html',
-		imageLink: './Art/hmahajan99/hmahajan99.png',
-		author: 'hmahajan99',
-		githubLink: 'https://github.com/hmahajan99'
-	},
-	{
-		artName: 'Lightning Strikes',
-		pageLink: './Art/Harshil/index.html',
-		imageLink: './Art/Harshil/lightning.PNG',
-		author: 'Harshil',
-		githubLink: 'https://github.com/funpractice'
-	},
+  {
+    artName: 'Bouncing Balls',
+    pageLink: './Art/varunv/canva.html',
+    imageLink: './Art/varunv/canva.gif',
+    author: 'Varun V',
+    githubLink: 'https://github.com/VarunV991'
+  },
+  {
+    artName: 'Russian Flag',
+    pageLink: './Art/AdityaSaroj/index.html',
+    imageLink: './Art/AdityaSaroj/russia.png',
+    author: 'Aditya Kumar Saroj',
+    githubLink: 'https://github.com/AdityaSaroj'
+  },
+  {
+    artName: 'Bauhaus Geometric',
+    pageLink: './Art/handa/index.html',
+    imageLink: './Art/handa/geometric.png',
+    author: 'Handa',
+    githubLink: 'https://github.com/handa26'
+  },
+  {
+    artName: 'Hacktoberfest rocks',
+    pageLink: './Art/cb/index.html',
+    imageLink: './Art/cb/hackto.png',
+    author: 'Chirag',
+    githubLink: 'https://github.com/bagdechirag'
+  },
+  {
+    artName: 'Gradient Hello World',
+    pageLink: './Art/mond/index.html',
+    imageLink: './Art/mond/pic.png',
+    author: 'Mond',
+    githubLink: 'https://github.com/monddelmundo'
+  },
+  {
+    artName: 'Bubble ZTM',
+    pageLink: './Art/ctq3103/bubble-ztm.html',
+    imageLink: './Art/ctq3103/bubble-ztm.gif',
+    author: 'ctq3103',
+    githubLink: 'https://github.com/ctq3103'
+  },
+  {
+    artName: 'Color Circle Anmination',
+    pageLink: './Art/tranjenny/index.html',
+    imageLink: './Art/tranjenny/theta.gif',
+    author: 'tranjenny',
+    githubLink: 'https://github.com/tranjenny'
+  },
+  {
+    artName: 'Spiraly Helix',
+    pageLink: './Art/hmahajan99/index.html',
+    imageLink: './Art/hmahajan99/hmahajan99.png',
+    author: 'hmahajan99',
+    githubLink: 'https://github.com/hmahajan99'
+  },
+  {
+    artName: 'Lightning Strikes',
+    pageLink: './Art/Harshil/index.html',
+    imageLink: './Art/Harshil/lightning.PNG',
+    author: 'Harshil',
+    githubLink: 'https://github.com/funpractice'
+  },
   {
     artName: 'Romanian Flag',
     pageLink: './Art/TigerAsH94/index.html',
@@ -322,6 +322,13 @@ let cards = [
 		imageLink: './Art/SquadTeemo/canvas_joy.jpg',
 		author: 'SquadTeemo',
 		githubLink: 'https://squadteemo.github.io/canvas-joy/'
+  },
+  {
+    artName: 'Flower Power',
+    pageLink: './Art/RedSquirrrel/index.html',
+    imageLink: './Art/RedSquirrrel/flover.gif',
+    author: 'RedSquirrrel',
+    githubLink: 'https://redsquirrrel.github.io/flover-style/'
   }
 ];
 
@@ -335,22 +342,26 @@ let cards = [
 // You don't need to modify this
 let contents = [];
 Shuffle(cards).forEach((c) => {
-	contents.push([
-		`<li class="card">` +
-			`<a href='${c.pageLink}' target="_blank">` +
-			`<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
-			`</a>` +
-			`<div class="flex-content">` +
-			`<a href='${c.pageLink}' target="_blank"><h3 class="art-title">${c.artName}</h3></a>` +
-			`<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
-			`</div>` +
-			`</li>`
-	]);
+  contents.push([
+    `<li class="card">` +
+      `<a href='${c.pageLink}' target="_blank">` +
+      `<img class="art-image" src='${c.imageLink}' alt='${c.artName}' />` +
+      `</a>` +
+      `<div class="flex-content">` +
+      `<a href='${c.pageLink}' target="_blank"><h3 class="art-title">${c.artName}</h3></a>` +
+      `<p class='author'><a href="${c.githubLink}" target="_blank"><i class="fab fa-github"></i> ${c.author}</a> </p>` +
+      `</div>` +
+      `</li>`
+  ]);
 });
 
 document.getElementById('cards').innerHTML = contents.join('');
 
 function Shuffle(o) {
-	for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-	return o;
+  for (
+    var j, x, i = o.length;
+    i;
+    j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+  );
+  return o;
 }
