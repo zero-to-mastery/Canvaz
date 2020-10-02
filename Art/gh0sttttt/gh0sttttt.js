305b2c24a311a5002b53a9eda03be2694cff2074
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2020-10-02 02:38:43 +0000

    (CI) format code

--- Art/gh0sttttt/gh0sttttt.js
+++ Art/gh0sttttt/gh0sttttt.js
@@ -5,22 +5,20 @@ const patternContext = patternCanvas.getContext('2d');
 const eye = patternCanvas.getContext('2d');
 const smile = patternCanvas.getContext('2d');
 
-
 function animate() {
   setInterval(init, 100);
 }
 function init() {
   patternCanvas.width = 150;
   patternCanvas.height = 150;
-  patternContext.fillStyle = "transparent";
+  patternContext.fillStyle = 'transparent';
   patternContext.fillRect(0, 0, patternCanvas.width, patternCanvas.height);
   patternContext.arc(75, 75, 50, 50, 0, Math.PI * 2, true);
   patternContext.stroke();
 
-
   eye.moveTo(55, 50);
   eye.beginPath();
-  eye.fillStyle = "black";
+  eye.fillStyle = 'black';
   // Left Eye
   eye.arc(50, 65, 5, 0, Math.PI * 2, true);
   eye.closePath();
@@ -32,7 +30,7 @@ function init() {
   // Smile
   smile.moveTo(105, 75);
   smile.beginPath();
-  smile.strokeStyle = "black";
+  smile.strokeStyle = 'black';
   smile.arc(75, 80, 10, 7, Math.PI, false);
   smile.stroke();
 
