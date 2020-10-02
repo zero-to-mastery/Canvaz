3e7cd8fb76921ca67a83b8ed2ee6ee1f67c58255
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2020-10-02 02:36:18 +0000

    (CI) format code

--- Art/cassio/script.js
+++ Art/cassio/script.js
@@ -11,12 +11,12 @@ draw.lineTo(200, 50);
 draw.lineTo(375, 150);
 draw.lineTo(200, 250);
 draw.lineTo(25, 150);
-draw.fillStyle = 'yellow'
+draw.fillStyle = 'yellow';
 draw.fill();
 
 draw.beginPath();
-draw.arc(200,150,70,0,2*Math.PI);
-draw.fillStyle = 'blue'
+draw.arc(200, 150, 70, 0, 2 * Math.PI);
+draw.fillStyle = 'blue';
 draw.fill();
 
 draw.beginPath();
