1243ae021f42bb2e9a483a30767e3b47c67145ee
Aszmel <mellipone@gmail.com>
2020-10-03 17:07:10 +0100

    first commit

---
+++ Art/Aszmel/script.js
@@ -0,0 +1,6 @@
+// first simple canvas to check if it's working ;)
+
+<script>
+  var c = document.getElementById("my_canvas"); var ctx = c.getContext("2d");
+  ctx.beginPath(); ctx.arc(95, 50, 40, 0, 2 * Math.PI); ctx.stroke();
+</script>;
