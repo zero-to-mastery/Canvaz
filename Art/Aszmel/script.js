47074f423c2ec5ee770c45af7cd14f66d0ed4114
Aszmel <mellipone@gmail.com>
2020-10-03 19:40:20 +0100

    faulty script tag removed

--- Art/Aszmel/script.js
+++ Art/Aszmel/script.js
@@ -1,6 +1,22 @@
 // first simple canvas to check if it's working ;)
 
-<script>
-  var c = document.getElementById("my_canvas"); var ctx = c.getContext("2d");
-  ctx.beginPath(); ctx.arc(95, 50, 40, 0, 2 * Math.PI); ctx.stroke();
-</script>;
+var c = document.getElementById('my_canvas');
+var ctx = c.getContext('2d');
+ctx.beginPath();
+ctx.arc(600, 250, 200, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(500, 250, 200, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.font = '50px Arial';
+ctx.strokeText('Hello Canvas', 10, 50);
+
+ctx.beginPath();
+ctx.arc(700, 250, 200, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(700, 250, 200, 0, 2 * Math.PI);
+ctx.stroke();
