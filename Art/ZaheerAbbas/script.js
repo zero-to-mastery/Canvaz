21f1ab1e4e47b21bb20729a56941845a93cb010f
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2020-10-03 23:23:36 +0000

    (CI) format code

--- Art/ZaheerAbbas/script.js
+++ Art/ZaheerAbbas/script.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("canvas");
+var canvas = document.getElementById('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -6,10 +6,10 @@ canvas.height = window.innerHeight;
 var ctx = canvas.getContext('2d');
 
 for (let j = 0; j < 10; j++) {
-    for (let i = 0; i < 10; i++) {
-        ctx.fillStyle = 'rgb(20, 200, 20)';
-        ctx.fillRect(j + i, j + i, 50, 50);
-        ctx.fillStyle = 'rgb(20, 150, 20)';
-        ctx.fillRect(i * 0.1 + j * 0.1, i * 0.1 + j * 0.1, 50, 50);
-    }
+  for (let i = 0; i < 10; i++) {
+    ctx.fillStyle = 'rgb(20, 200, 20)';
+    ctx.fillRect(j + i, j + i, 50, 50);
+    ctx.fillStyle = 'rgb(20, 150, 20)';
+    ctx.fillRect(i * 0.1 + j * 0.1, i * 0.1 + j * 0.1, 50, 50);
+  }
 }
