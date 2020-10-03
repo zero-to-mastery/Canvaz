0191234653ea37100c634dedcda9933bae01eb5a
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2020-10-03 22:33:18 +0000

    (CI) format code

--- Art/giuffrida-canvas/script.js
+++ Art/giuffrida-canvas/script.js
@@ -1,25 +1,17 @@
 function draw() {
-	var image = document.querySelector('img');
-	var canvas = document.querySelector('canvas');
+  var image = document.querySelector('img');
+  var canvas = document.querySelector('canvas');
 
-	var ctx = canvas.getContext('2d');
+  var ctx = canvas.getContext('2d');
 
+  ctx.fillRect(0, 0, 320, 320);
 
-	ctx.fillRect(0, 0, 320, 320);
+  ctx.fillStyle = '#989898';
+  ctx.fillRect(10, 10, 300, 300);
 
-	ctx.fillStyle = '#989898';
-	ctx.fillRect(10, 10, 300, 300);
-
-	ctx.drawImage(
-		image, 
-		0, 0,
-		500, 500,
-		0, 0,
-		320, 320
-	)
+  ctx.drawImage(image, 0, 0, 500, 500, 0, 0, 320, 320);
 }
 
-
 setTimeout(() => {
-	draw();
-}, 300)
+  draw();
+}, 300);
