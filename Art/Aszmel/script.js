0fac859ced1426fcadbc39c36fe2793d95d7a6bb
Aszmel <mellipone@gmail.com>
2020-10-04 18:14:14 +0100

    drawing png into my canvas, screentshot, still to put my work into include

--- Art/Aszmel/script.js
+++ Art/Aszmel/script.js
@@ -1,22 +1,33 @@
-// first simple canvas to check if it's working ;)
+// var c = document.getElementById('my_canvas');
+// var ctx = c.getContext('2d');
+// ctx.beginPath();
+// ctx.arc(600, 250, 200, 0, 2 * Math.PI);
+// ctx.stroke();
 
-var c = document.getElementById('my_canvas');
-var ctx = c.getContext('2d');
-ctx.beginPath();
-ctx.arc(600, 250, 200, 0, 2 * Math.PI);
-ctx.stroke();
+// ctx.beginPath();
+// ctx.arc(500, 250, 200, 0, 2 * Math.PI);
+// ctx.stroke();
 
-ctx.beginPath();
-ctx.arc(500, 250, 200, 0, 2 * Math.PI);
-ctx.stroke();
+// ctx.font = '50px Arial';
+// ctx.strokeText('Hello Canvas', 10, 50);
 
-ctx.font = '50px Arial';
-ctx.strokeText('Hello Canvas', 10, 50);
+// ctx.beginPath();
+// ctx.arc(700, 250, 200, 0, 2 * Math.PI);
+// ctx.stroke();
 
-ctx.beginPath();
-ctx.arc(700, 250, 200, 0, 2 * Math.PI);
-ctx.stroke();
+// ctx.beginPath();
+// ctx.arc(700, 250, 200, 0, 2 * Math.PI);
+// ctx.stroke();
 
-ctx.beginPath();
-ctx.arc(700, 250, 200, 0, 2 * Math.PI);
-ctx.stroke();
+window.addEventListener('DOMContentLoaded', function () {
+  var image = document.getElementById('html5');
+  var canvas = document.createElement('canvas');
+  document.body.appendChild(canvas);
+
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  var context = canvas.getContext('2d');
+
+  context.drawImage(image, 0, 0, 50, 50);
+});
