87681ad4cc598bb97ef238ad5fe279448149fabf
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2020-10-04 17:49:11 +0000

    (CI) format code

--- Art/joedag32/scripts.js
+++ Art/joedag32/scripts.js
@@ -1,9 +1,9 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 
-Math.radians = function(degrees) {
-	return degrees * Math.PI / 180;
-}
+Math.radians = function (degrees) {
+  return (degrees * Math.PI) / 180;
+};
 Math.radians(90);
 
 // left eye
@@ -12,7 +12,7 @@ ctx.arc(300, 200, 40, 0, 2 * Math.PI);
 ctx.stroke();
 ctx.beginPath();
 ctx.arc(310, 210, 15, 0, 2 * Math.PI);
-ctx.fillStyle = "#000";
+ctx.fillStyle = '#000';
 ctx.fill();
 // right eye
 ctx.beginPath();
@@ -20,12 +20,12 @@ ctx.arc(400, 200, 40, 0, 2 * Math.PI);
 ctx.stroke();
 ctx.beginPath();
 ctx.arc(390, 210, 15, 0, 2 * Math.PI);
-ctx.fillStyle = "#000";
+ctx.fillStyle = '#000';
 ctx.fill();
 // nose
 ctx.beginPath();
 ctx.arc(350, 230, 30, 0, 2 * Math.PI);
-ctx.fillStyle = "#000";
+ctx.fillStyle = '#000';
 ctx.fill();
 // smile
 ctx.beginPath();
