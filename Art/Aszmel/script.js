061112a6566dc676c43991eb1c70eaeb410911d5
Aszmel <mellipone@gmail.com>
2020-10-04 18:20:49 +0100

    cleaning code, centered png picture

--- Art/Aszmel/script.js
+++ Art/Aszmel/script.js
@@ -1,24 +1,3 @@
-// var c = document.getElementById('my_canvas');
-// var ctx = c.getContext('2d');
-// ctx.beginPath();
-// ctx.arc(600, 250, 200, 0, 2 * Math.PI);
-// ctx.stroke();
-
-// ctx.beginPath();
-// ctx.arc(500, 250, 200, 0, 2 * Math.PI);
-// ctx.stroke();
-
-// ctx.font = '50px Arial';
-// ctx.strokeText('Hello Canvas', 10, 50);
-
-// ctx.beginPath();
-// ctx.arc(700, 250, 200, 0, 2 * Math.PI);
-// ctx.stroke();
-
-// ctx.beginPath();
-// ctx.arc(700, 250, 200, 0, 2 * Math.PI);
-// ctx.stroke();
-
 window.addEventListener('DOMContentLoaded', function () {
   var image = document.getElementById('html5');
   var canvas = document.createElement('canvas');
@@ -29,5 +8,5 @@ window.addEventListener('DOMContentLoaded', function () {
 
   var context = canvas.getContext('2d');
 
-  context.drawImage(image, 0, 0, 50, 50);
+  context.drawImage(image, 10, 10);
 });
