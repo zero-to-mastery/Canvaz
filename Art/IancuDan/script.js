86195ce57b3161c5198d13ed9f9e56db1f7a268e
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2020-10-14 02:10:44 +0000

    (CI) format code

--- Art/IancuDan/script.js
+++ Art/IancuDan/script.js
@@ -1,10 +1,10 @@
-var c = document.getElementById("fishCanvas");
-var ctx = c.getContext("2d");
+var c = document.getElementById('fishCanvas');
+var ctx = c.getContext('2d');
 ctx.beginPath();
 ctx.moveTo(20, 50);
 ctx.quadraticCurveTo(50, 140, 200, 50);
 ctx.stroke();
-ctx.fillStyle ='green';
+ctx.fillStyle = 'green';
 ctx.fill();
 
 ctx.moveTo(20, 50);
@@ -12,9 +12,8 @@ ctx.quadraticCurveTo(50, 0, 200, 70);
 ctx.stroke();
 ctx.fill();
 
-
 ctx.beginPath();
 ctx.arc(60, 60, 10, 0, 2 * Math.PI);
 ctx.stroke();
-ctx.fillStyle ='black';
+ctx.fillStyle = 'black';
 ctx.fill();
