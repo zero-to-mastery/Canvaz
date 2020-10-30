6c0a32ffec264a2e5fe227dbebd03b9ee3a9c9c1
Rabanov <agy.rodrigues@gmail.com>
2020-10-30 17:07:56 -0400

    ghost body done
    -ghost top done
    -ghost bottom done
    -left and right tooth
    -mouth
    -left and right eye

--- Art/Rabanov/Rabanov.js
+++ Art/Rabanov/Rabanov.js
@@ -2,26 +2,43 @@ window.onload = function() {
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 
+//left eye
+ctx.beginPath();
+ctx.moveTo(60,35);
+ctx.quadraticCurveTo(55,55,70,50);
+ctx.fill();
+
+//right eye
+ctx.beginPath();
+ctx.moveTo(100,35);
+ctx.quadraticCurveTo(105,55,90,50);
+ctx.fill();
+
+//left tooth
 ctx.beginPath();
 ctx.moveTo(60, 70);
 ctx.lineTo(70, 70);
 ctx.lineTo(70, 90);
 ctx.fill();
 
+//mouth
 ctx.moveTo(55, 80)
 ctx.bezierCurveTo(68, 60, 90, 60, 105, 80)
 ctx.stroke();
 
+//right tooth
 ctx.beginPath();
 ctx.moveTo(90, 70);
 ctx.lineTo(100, 70);
 ctx.lineTo(90, 90);
 ctx.fill();
 
+//ghost body top
 ctx.moveTo(30,120)
 ctx.quadraticCurveTo(75, -100, 135,120)
 ctx.stroke();
 
+//zig-zag ghost body bottom
 ctx.beginPath();
 ctx.moveTo(30, 120);
 ctx.lineTo(56.25, 100 );
