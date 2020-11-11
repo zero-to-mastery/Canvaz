ea4ad0f4a8a017b5eaca5f50018df4f97622bcc9
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2020-11-11 21:10:57 +0000

    (CI) format code

--- Art/krishna/script.js
+++ Art/krishna/script.js
@@ -1,36 +1,36 @@
-var canvas = document.querySelector('canvas')
+var canvas = document.querySelector('canvas');
 
-canvas.width = window.innerWidth * .9
-canvas.height = window.innerHeight * .9
+canvas.width = window.innerWidth * 0.9;
+canvas.height = window.innerHeight * 0.9;
 
-var context = canvas.getContext('2d')
+var context = canvas.getContext('2d');
 
-for (let i=0; i < 10; i++) {
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
-    context.beginPath()
-    context.arc(x, y, 70, 0, Math.PI * 2, false)
-    context.strokeStyle = 'blue'
-    context.stroke()
+for (let i = 0; i < 10; i++) {
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+  context.beginPath();
+  context.arc(x, y, 70, 0, Math.PI * 2, false);
+  context.strokeStyle = 'blue';
+  context.stroke();
 }
 
-for (let i=0; i < 10; i++) {
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
-    context.beginPath()
-    context.rect(x, y, 200, 150)
-    context.strokeStyle = 'green'
-    context.stroke()
+for (let i = 0; i < 10; i++) {
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+  context.beginPath();
+  context.rect(x, y, 200, 150);
+  context.strokeStyle = 'green';
+  context.stroke();
 }
 
-for (let i=0; i < 1; i++) {
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
-    context.beginPath();
-    context.moveTo(x, x);
-    context.lineTo(x, y);
-    context.lineTo(y, y);
-    context.strokeStyle = 'red'
-    context.closePath();
-    context.stroke()
+for (let i = 0; i < 1; i++) {
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+  context.beginPath();
+  context.moveTo(x, x);
+  context.lineTo(x, y);
+  context.lineTo(y, y);
+  context.strokeStyle = 'red';
+  context.closePath();
+  context.stroke();
 }
