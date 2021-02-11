ac389fcfbae1e9570a03281eeb11c316bdd95b80
Zero to Mastery <zero-to-mastery@users.noreply.github.com>
2021-02-11 22:05:03 +0000

    (CI) format code

--- Art/DaveBhandari/script5.js
+++ Art/DaveBhandari/script5.js
@@ -1,11 +1,11 @@
-var canvas = document.getElementById("myCanvas");
-var ctx=canvas.getContext("2d");
-ctx.font = "30px Arial";
-ctx.strokeStyle = "white";
-ctx.fillStyle = "red";
-ctx.textAlign = "center";
-ctx.strokeText("ZTM",canvas.width/2, canvas.height/2);
-ctx.fillText("Zero to Mastery",200,200);
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+ctx.font = '30px Arial';
+ctx.strokeStyle = 'white';
+ctx.fillStyle = 'red';
+ctx.textAlign = 'center';
+ctx.strokeText('ZTM', canvas.width / 2, canvas.height / 2);
+ctx.fillText('Zero to Mastery', 200, 200);
 
 // Create gradient
 //var grd = ctx.createRadialGradient(75,50,5,90,60,500);
@@ -16,10 +16,10 @@ ctx.fillText("Zero to Mastery",200,200);
 //ctx.fillStyle = grd;
 //ctx.fillRect(0,0,400,300);
 
-ctx.moveTo(0,170);
-ctx.lineTo(400,170);
+ctx.moveTo(0, 170);
+ctx.lineTo(400, 170);
 ctx.stroke();
 
-ctx.moveTo(0,220);
-ctx.lineTo(400,220);
+ctx.moveTo(0, 220);
+ctx.lineTo(400, 220);
 ctx.stroke();
