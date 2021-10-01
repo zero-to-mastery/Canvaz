39e4f16d902f55f72b12f8dc8b8ee4acb911f478
brittneypostma <bdesignedwebdev@gmail.com>
2021-10-01 12:06:15 +0000

    Deploying to gh-pages from  @ 3c794bd586adb7989b503c98049e75b620a7db42 🚀

---
+++ art/haylzrandom/script.js
@@ -0,0 +1,79 @@
+const c = document.getElementById('canvas');
+const ctx = c.getContext('2d');
+
+// Canvas Dimensions
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+c.width = width;
+c.height = height;
+
+let maxFlakes = 55; // max number of snowflakes on screen
+let flakes = [];
+
+for (let i = 0; i < maxFlakes; i++) {
+	flakes.push({
+		x: Math.random() * width, // x-coord
+		y: Math.random() * height, // y-coord
+		r: Math.random() * 5 + 2, // radius
+		d: Math.random() * maxFlakes, // density
+	});
+}
+
+// Function that will draw snowflakes on canvas
+const drawFlakes = () => {
+	ctx.clearRect(0, 0, width, height);
+
+	ctx.fillStyle = 'rgba(255, 255, 255, 0.7)';
+	ctx.beginPath();
+
+	for (let j = 0; j < maxFlakes; j++) {
+		let flake = flakes[j];
+		ctx.moveTo(flake.x, flake.y);
+		ctx.arc(flake.x, flake.y, flake.r, 0, Math.PI * 2, true);
+	}
+	ctx.fill();
+	update();
+};
+
+let angle = 0;
+
+// Moving the snowflakes
+const update = () => {
+	angle += 0.01;
+
+	for (let h = 0; h < maxFlakes; h++) {
+		let flake = flakes[h];
+		flake.y += Math.cos(angle + flake.d) + 1 + flake.r / 2;
+		flake.x += Math.sin(angle) * 2;
+
+		if (flake.x > width + 5 || flake.x < -5 || flake.y > height) {
+			if (h % 3 > 0) {
+				flakes[h] = {
+					x: Math.random() * width,
+					y: -10,
+					r: flake.r,
+					d: flake.d,
+				};
+			} else {
+				if (Math.sin(angle) > 0) {
+					flakes[h] = {
+						x: -5,
+						y: Math.random() * height,
+						r: flake.r,
+						d: flake.d,
+					};
+				} else {
+					flakes[h] = {
+						x: width + 5,
+						y: Math.random() * height,
+						r: flake.r,
+						d: flake.d,
+					};
+				}
+			}
+		}
+	}
+};
+
+setInterval(drawFlakes, 40);
