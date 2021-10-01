9fb6ab5b5bf761dfe5ea451cce4a1ba0307756f5
MattCSmith <matt@matt-smith.dev>
2021-10-01 20:04:14 +0000

    Deploying to gh-pages from  @ 82e6bbe2c6e2371951d553b51cb3efa04b2e2cf6 🚀

---
+++ art/JotSinghChawla/script.js
@@ -0,0 +1,11 @@
+
+var c = document.getElementById("flag_canvas");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#FF9933";
+ctx.fillRect(0, 0, 270, 60);
+
+ctx.fillStyle = "white"; 
+ctx.fillRect(0, 60, 270, 60); 
+
+ctx.fillStyle = "#138808"; 
+ctx.fillRect(0, 120, 270, 60);
