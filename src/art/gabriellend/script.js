6c1417e8768e600c85aa628b75a6f2d12f664da2
Gabrielle Davidson <gdvdsn@gmail.com>
2021-10-02 12:29:37 -1000

    Added animation

--- src/art/gabriellend/script.js
+++ src/art/gabriellend/script.js
@@ -1,34 +1,67 @@
-const draw = () => {
-  let canvas = document.getElementById('myCanvas');
-  if (canvas.getContext) {
-    let ctx = canvas.getContext('2d');
-    let moonBlue = '#182B4E';
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
 
-    ctx.beginPath();
-    ctx.arc(150, 150, 60, (Math.PI/180)*40, (Math.PI/180)*320);
-    //create a loop to go up and down phases
-    ctx.bezierCurveTo(80, 50, 80, 250, 196, 188);
-    ctx.strokeStyle = 'white';
-    ctx.stroke();
-    ctx.save();
+const init = () => {
+  ctx.clearRect(75, 75, 150, 150);
+  ctx.beginPath();
+  ctx.arc(150, 150, 60, (Math.PI/180)*40, (Math.PI/180)*320);
+  ctx.bezierCurveTo(80, 50, 80, 250, 197, 188);
+  ctx.fillStyle = 'white';
+  ctx.fill();
 
-    ctx.beginPath();
-    ctx.arc(150, 150, 60, (Math.PI/180)*40, (Math.PI/180)*320);
-    //create a loop to go up and down phases
-    ctx.bezierCurveTo(80, 50, 80, 250, 196, 188);
-    ctx.fillStyle = 'white';
-    ctx.fill();
-    // ctx.save();
+  setTimeout(quarterMoon, 1000);
+};
 
-    // clearRect();
+const quarterMoon = () => {
+  ctx.clearRect(75, 75, 150, 150);
+  ctx.beginPath();
+  ctx.arc(150, 150, 60, (Math.PI/180)*40, (Math.PI/180)*320);
+  ctx.bezierCurveTo(110, 75, 110, 225, 196, 188);
+  ctx.fillStyle = 'white';
+  ctx.fill();
 
-    // ctx.beginPath();
-    // ctx.arc(150, 150, 60, (Math.PI/180)*40, (Math.PI/180)*320);
-    // ctx.bezierCurveTo(150, 50, 150, 250, 196, 188);
-    // ctx.strokeStyle = 'white';
-    // ctx.stroke();
-    // ctx.save();
-  }
-}
+  setTimeout(halfMoon, 125);
+};
 
-// setInterval(draw, 1000);
+const halfMoon = () => {
+  ctx.clearRect(75, 75, 150, 150);
+  ctx.beginPath();
+  ctx.arc(150, 150, 60, (Math.PI/180)*30, (Math.PI/180)*330);
+  ctx.bezierCurveTo(140, 110, 140, 190, 201, 182);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+
+  setTimeout(fullMoon, 125);
+};
+
+const fullMoon = () => {
+  ctx.clearRect(75, 75, 150, 150);
+  ctx.beginPath();
+  ctx.arc(150, 150, 60, (Math.PI/180)*30 , (Math.PI/180)*390);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+
+  setTimeout(halfMoonWane, 1000);
+};
+
+const halfMoonWane = () => {
+  ctx.clearRect(75, 75, 150, 150);
+  ctx.beginPath();
+  ctx.arc(150, 150, 60, (Math.PI/180)*30, (Math.PI/180)*330);
+  ctx.bezierCurveTo(140, 110, 140, 190, 201, 182);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+
+  setTimeout(quarterMoonWane, 125);
+};
+
+const quarterMoonWane = () => {
+  ctx.clearRect(75, 75, 150, 150);
+  ctx.beginPath();
+  ctx.arc(150, 150, 60, (Math.PI/180)*40, (Math.PI/180)*320);
+  ctx.bezierCurveTo(110, 75, 110, 225, 196, 188);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+
+  setTimeout(init, 125);
+};
