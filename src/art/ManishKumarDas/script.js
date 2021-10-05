422ff7ff56efe55a51779b0e39f88509a7ca70f8
Manish Kumar Das <manish.das@krishi.network>
2021-10-05 20:04:04 +0530

    updated the art

--- src/art/ManishKumarDas/script.js
+++ src/art/ManishKumarDas/script.js
@@ -2,37 +2,59 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-// this is the smallest dimension of the screen.
-// this will help set up the canvas square with the least dimension as the square side.
-let CANVAS_SIDE, GAP, SHAPE_WIDTH, SHAPE_HEIGHT, NO_OF_SIDES
-NO_OF_SIDES = 5
+const gap = 20
+const width = 100
 
-// this is the gap at the end of the screen * 2
-const WINDOW_MARGIN = 50
+const colors = ['royalblue', 'yellow', '#6a26cd', '#fd019c', '#00ff22']
 
-const canvasResize = () => {
-
-    CANVAS_SIDE = window.innerWidth < window.innerHeight 
-                    ? window.innerWidth : window.innerHeight
-
-    // considering we have 5 rows and 5 columns and the shape_width and shape_height is 2 times the gap.
-    // that gives us the equation:        (no_of_sides) * shape_width + (no_of_sides + 1) * gap = canvas_side
-    //                              =>    (no_of_sides) * ( 2 * gap ) + (no_of_sides + 1) * gap = canvas_side
-    //                              =>        (2 * no_of_sides) * gap + (no_of_sides + 1) * gap = canvas_side
-    //                              =>                              (3 * no_of_sides + 1) * gap = canvas_side
-    //                              =>                                                      gap = canvas_side / (3 * no_of_sides + 1)
-    //                              =>                                shape_width, shape_height = 2 * canvas_side / (3 * no_of_sides + 1)
-    
-    GAP             =       CANVAS_SIDE / (3 * NO_OF_SIDES + 1)
-    SHAPE_WIDTH     = 2 *   CANVAS_SIDE / (3 * NO_OF_SIDES + 1)
-    SHAPE_HEIGHT    = 2 *   CANVAS_SIDE / (3 * NO_OF_SIDES + 1)
-
-    canvas.width    = CANVAS_SIDE - WINDOW_MARGIN
-    canvas.height   = CANVAS_SIDE - WINDOW_MARGIN
+const pickRandomColor = () => {
+    const choice = Math.floor(Math.random() * colors.length)
+    return colors[choice]
 }
 
-// resize the canvas
-canvasResize()
-window.addEventListener('resize', canvasResize)
+const drawRect = (x, y, width, height, stroke_color, stroke_width) => {
+    ctx.beginPath()
+    ctx.rect(x, y, width, height)
 
-// draw
+    pickRandomColor()
+    
+    ctx.fillStyle = pickRandomColor()
+    ctx.strokeStyle = pickRandomColor()
+    
+    // Math.random() > 0.5 ? ctx.stroke() : ctx.fill()
+    ctx.fill()
+}
+
+const drawCircle = (x, y, radius, fill) => {
+    ctx.beginPath()
+    ctx.arc(x, y, radius, 0, 2 * Math.PI)
+    
+    ctx.fillStyle = pickRandomColor()
+    ctx.strokeStyle = pickRandomColor()
+    
+    // Math.random() > 0.5 ? ctx.stroke() : ctx.fill()
+    ctx.fill()
+}
+
+const drawShapes = () => {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    
+    for (i = 0; i < 5; i++) {
+
+        for(j = 0; j < 5; j++) {
+            
+            x = gap + (gap + width) * i
+            y = gap + (gap + width) * j
+            
+            Math.random() > 0.5 
+                ? drawRect(x, y, width, width)
+                : drawCircle(x + width / 2, y + width / 2, width / 2)
+        }
+
+    }
+}
+
+drawShapes()
+
+setInterval(drawShapes, 1000)
