082e31749d5ee6e05fe12b5e2eb7ce477df1dc16
anirban-s <baban.chrome@gmail.com>
2021-10-11 15:59:56 +0530

    my purple star

---
+++ src/art/anirbansarkar4/script.js
@@ -0,0 +1,18 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "#b800e6";
+ctx.beginPath();
+ctx.moveTo(108, 0.0);
+ctx.lineTo(141, 70);
+ctx.lineTo(218, 78.3);
+ctx.lineTo(162, 131);
+ctx.lineTo(175, 205);
+ctx.lineTo(108, 170);
+ctx.lineTo(41.2, 205);
+ctx.lineTo(55, 131);
+ctx.lineTo(1, 78);
+ctx.lineTo(75, 68);
+ctx.lineTo(108, 0);
+ctx.closePath();
+ctx.fill();
