c383200d0ecd6ae9c9d7772906bd22fd52eafee4
Bryce Drawe <brycerdrawe.b@gmail.com>
2021-10-13 10:53:11 -0600

    changes to index.js

--- src/art/bryceDrawe/index.js
+++ src/art/bryceDrawe/index.js
@@ -1,68 +1,6 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-// //fillRect()
-// ctx.fillStyle = 'red'
-// ctx.fillRect(20,20,150,100);
-// ctx.fillStyle ="blue"
-// ctx.fillRect(200,20,150,100);
-
-// //strokeRect()
-// ctx.lineWidth ="5"
-// ctx.strokeStyle ="green"
-// ctx.strokeRect(100,200, 150, 100);
-
-// //clearRect()
-// ctx.clearRect(25,25,140,90);
-
-// //fillText()
-// ctx.font ="30px Arial"
-// ctx.fillStyle="purple"
-// ctx.fillText('Hello World',400,50)
-
-// //strokeText()
-// ctx.lineWidth = 1;
-// ctx.strokeText('Hello World', 400, 100)
-
-//Paths
-
-// ctx.beginPath();
-// ctx.moveTo(50,50);
-// ctx.lineTo(150,50);
-// ctx.lineTo(100,200)
-// // ctx.lineTo(50,50)
-// ctx.closePath();
-// // ctx.stroke();
-// ctx.fillStyle ='coral'
-// ctx.fill();
-
-// ctx.beginPath()
-// ctx.moveTo(200,50);
-// ctx.lineTo(150,200);
-// ctx.lineTo(250,200);
-// ctx.closePath();
-// ctx.stroke();
-
-// ctx.beginPath()
-// ctx.rect(300,50,150,100)
-// ctx.fillStyle='teal'
-// ctx.fill();
-
-//Arc(circles)
-// ctx.beginPath()
-// const centerX = canvas.width / 2;
-// const centerY = canvas.height / 2;
-
-// ctx.arc(centerX, centerY, 200,0,Math.PI * 2);
-// ctx.moveTo(centerX + 100, centerY);
-// ctx.arc(centerX, centerY, 100,0,Math.PI, false);
-// ctx.moveTo(centerX-60, centerY - 80);
-// ctx.arc(centerX - 80, centerY - 80, 20, 0 , Math.PI * 2)
-// ctx.moveTo(centerX + 100, centerY - 80)
-// ctx.arc(centerX + 80, centerY - 80, 20, 0 , Math.PI * 2)
-// ctx.stroke();
-
-//animation 1
 const circle1 = {
 	x: 200,
 	y: 100,
