e9a4546c7f360943d48f839de460083eba1d3319
Joseph <ralphlsulit@gmail.com>
2021-10-25 16:07:10 +0800

    2nd feature (squidGame mask)

--- src/art/sulit/script.js
+++ src/art/sulit/script.js
@@ -28,3 +28,29 @@ p.beginPath();
 p.rect(670, 375, 65, 65);
 p.lineWidth = 7;
 p.stroke();
+
+//1st column
+p.beginPath();
+p.fillStyle = '#FF0000';
+p.fillRect(100, 400, 200, 300);
+p.closePath();
+
+//1st column elipse
+function Oval(x, y, radiusX, radiusY, rotation) {
+  p.beginPath();
+  p.fillStyle = '#000';
+  p.ellipse(x, y, radiusX, radiusY, rotation, 0, 2 * Math.PI);
+  p.fill();
+}
+
+let deg = 90;
+let rad = deg * (Math.PI / 180.0);
+
+Oval(200, 520, 70, 50, rad);
+
+//1st column circle
+p.beginPath();
+p.strokeStyle = '#fff';
+p.arc(200, 504, 30, 0, 2 * Math.PI);
+p.lineWidth = 10;
+p.stroke();
