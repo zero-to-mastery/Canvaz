c9e4cb439c26b07197d16507b3e56d09aadb09f6
dimitrisz123 <dimitris-007@hotmail.com>
2021-10-31 16:23:02 +0200

    update

--- src/art/dimitrisz123/app.js
+++ src/art/dimitrisz123/app.js
@@ -1,48 +1,7 @@
-var c = document.getElementById("mycanvas");
-        c.width = window.innerWidth;
-        c.height = window.innerHeight;
-        var $ = c.getContext('2d');
-        var numbers = 600;
-        var p = [];
-        console.log((Math.random() * 2));
-
-        function create() {
-            for (var i = 0; i < numbers; i++) {
-                var d = {
-                    x: Math.random() * c.width
-                    , y: Math.random() * c.height
-                    , vy: (Math.random() * 10) + 5
-                }
-                p.push(d);
-            }
-        }
-
-        function update() {
-            base_image = new Image();
-            base_image.src = 'icon.png';
-            $.drawImage(base_image, 0, 0, c.width,c.height);
-
-            for (var i = 0; i < numbers; i++) {
-                p[i].y += p[i].vy;
-                if (p[i].y + 10 > c.height || p[i].y < 0) {
-                    p[i].y = 0;
-                }
-
-                $.fillStyle = "white";
-                $.globalAlpha = 0.6;
-
-                $.fillRect(p[i].x, p[i].y, 1, 15);
-            }
-        }
-        window.requestAnimFrame = (function () {
-            return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
-                window.setTimeout(callback, 1000 / 60);
-            };
-        })();
-
-        function run() {
-            update();
-            window.requestAnimFrame(run);
-        }
-        create();
-        window.requestAnimFrame(run);
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var imageObj = new Image();
+imageObj.src = "icon.png";
+imageObj.onload = function () {
+  context.drawImage(imageObj, 0, 0);
+};
